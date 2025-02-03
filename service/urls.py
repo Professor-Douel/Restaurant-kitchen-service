de1cbc68337a0a160285361a5bd6493ebd3c066c
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 16:41:03 +0200

    add paths to dish-create & dish-detail

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 
-from service.views import index, DishesListView
+from service.views import index, DishesListView, DishCreateView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("dishes/", DishesListView.as_view(), name="dishes-list"),
+    path("dishes/", DishesListView.as_view(), name="dishes"),
+    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
     path("dish/", DishDetailView.as_view(), name="dish-detail"),
 ]
 

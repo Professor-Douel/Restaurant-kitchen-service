8ff6d932254d5383868dcf51eec291a39a69b0e3
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 16:09:43 +0200

    add path to dish-detail in file service/urls.py

--- service/urls.py
+++ service/urls.py
@@ -6,6 +6,7 @@ from service.views import index, DishesListView
 urlpatterns = [
     path("", index, name="index"),
     path("dishes/", DishesListView.as_view(), name="dishes-list"),
+    path("dish/", DishDetailView.as_view(), name="dish-detail"),
 ]
 
 app_name = "service"

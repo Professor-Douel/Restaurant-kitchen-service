ee3033f8489fa43c5e9c3e478ed9bfe4dd7871fa
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 13:32:32 +0200

    add fields groups and user_permissions to the model Cook in file models.py

--- service/models.py
+++ service/models.py
@@ -11,6 +11,16 @@ class DishType(models.Model):
 
 class Cook(AbstractUser):
     years_of_experience = models.PositiveIntegerField()
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="+",
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="+",
+        blank=True
+    )
 
     def __str__(self):
         return f"{self.username} ({self.years_of_experience} років досвіду)"

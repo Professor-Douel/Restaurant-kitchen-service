ecda4682c76467a2d0e4b800f12fee9360597324
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 15:13:37 +0200

    add the function index to file views.py

--- service/views.py
+++ service/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from service.models import Dish
+
+
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    num_dishes = Dish.objects.all().count()
+
+    context = {
+        'num_dishes': num_dishes,
+    }
+    return render(request, "service/index.html", context=context)

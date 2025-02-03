9abf60d158536a98f30744519685e9df2954ef8a
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 18:05:50 +0200

    add paths dish-detail, dish-update, dish-delete

--- service/urls.py
+++ service/urls.py
@@ -1,13 +1,16 @@
 
 from django.urls import path
 
-from service.views import index, DishesListView, DishCreateView, DishDetailView
+from service.views import index, DishesListView, DishCreateView, DishDetailView, \
+    DishUpdateView, DishDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
     path("dishes/", DishesListView.as_view(), name="dishes"),
     path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
-    path("dish/", DishDetailView.as_view(), name="dish-detail"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
 ]
 
 app_name = "service"

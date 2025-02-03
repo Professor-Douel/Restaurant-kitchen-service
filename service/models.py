407251030668ac2abe4b4f67b3da2478bc5f58e9
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 12:42:38 +0200

    add model Cook to models.py

--- service/models.py
+++ service/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
@@ -6,3 +7,10 @@ class DishType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.username} ({self.years_of_experience} років досвіду)"

137f7f6e7cd6d202cd9b413fd221799e358086ad
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 12:51:19 +0200

    add model Dish to models.py

--- service/models.py
+++ service/models.py
@@ -14,3 +14,14 @@ class Cook(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.years_of_experience} років досвіду)"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return self.name

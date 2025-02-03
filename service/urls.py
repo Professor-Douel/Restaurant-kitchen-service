d22f4691b906a9f708b5aea4d2d77bda8f8299a8
Oleksandr Barabanov <imtence@icloud.com>
2025-02-03 15:54:45 +0200

    add path to dishes to file service/urls.py

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
 
-from service.views import index
-
+from service.views import index, DishesListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dishes/", DishesListView.as_view(), name="dishes-list"),
 ]
 
 app_name = "service"

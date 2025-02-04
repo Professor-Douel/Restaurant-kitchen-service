8f589a7ce1080fccadceb694c571fd1649dc9d0b
Oleksandr Barabanov <imtence@icloud.com>
2025-02-04 12:05:50 +0200

    fix code style in service/urls.py

--- service/urls.py
+++ service/urls.py
@@ -1,17 +1,51 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from service.views import index, DishesListView, DishCreateView, DishDetailView, \
-    DishUpdateView, DishDeleteView
+from service.views import (
+    index,
+    DishesListView,
+    DishCreateView,
+    DishDetailView,
+    DishUpdateView,
+    DishDeleteView
+)
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("dishes/", DishesListView.as_view(), name="dishes"),
-    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
-    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
-    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
-    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
-    path("login/", LoginView.as_view(), name="login"),
+    path(
+        "",
+        index,
+        name="index"
+    ),
+    path(
+        "dishes/",
+        DishesListView.as_view(),
+        name="dishes"
+    ),
+    path(
+        "dishes/create/",
+        DishCreateView.as_view(),
+        name="dish-create"
+    ),
+    path(
+        "dish/<int:pk>/",
+        DishDetailView.as_view(),
+        name="dish-detail"
+    ),
+    path(
+        "dish/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update"
+    ),
+    path(
+        "dish/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete"
+    ),
+    path(
+        "login/",
+        LoginView.as_view(),
+        name="login"
+    ),
 ]
 
 app_name = "service"

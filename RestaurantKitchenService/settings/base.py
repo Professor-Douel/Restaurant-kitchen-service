cc572d0890cf7b0c8ea28a72b7f716f0cd6c3421
Oleksandr Barabanov <imtence@icloud.com>
2025-02-11 02:36:19 +0200

    update readme.md

--- RestaurantKitchenService/settings/base.py
+++ RestaurantKitchenService/settings/base.py
@@ -80,10 +80,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "service/index"
+LOGIN_REDIRECT_URL = "/"
 
-LOGOUT_REDIRECT_URL = "login"
+LOGOUT_REDIRECT_URL = "/"
 
-REGISTER_REDIRECT_URL = "service/index"
+REGISTER_REDIRECT_URL = "/"
 
 AUTH_USER_MODEL = "service.Cook"
